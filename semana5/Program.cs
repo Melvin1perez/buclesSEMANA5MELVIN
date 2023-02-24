147edf00b1b012ab7ab2c2478cfaf1611a192b19
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2023-02-23 18:51:04 -0600

    Add files via upload

---
+++ semana5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Solicitar valor inicial y final al usuario
+        Console.Write("Ingrese valor inicial: ");
+        int inicio = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese valor final: ");
+        int fin = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Números pares entre {0} y {1}:", inicio, fin);
+
+        // Iniciar puntero en el valor inicial y avanzar de dos en dos hasta el valor final
+        int i = inicio;
+        while (i <= fin) {
+            if (i % 2 == 0) {
+                Console.WriteLine(i);
+            }
+            i++;
+        }
+    }
+}
+
